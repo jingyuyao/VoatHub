188968905d03a7041debe09253d49a7eed6ff1d0
Jingyu Yao <yjy.emails@gmail.com>
2015-08-06 19:20:35 -0400

    Added missiong files.

---
+++ VoatHub/Data/UserSubmission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoatHub.Data
+{
+    public class UserSubmission
+    {
+        public string title { get; set; }
+        public bool nsfw { get; set; }
+        public bool anon { get; set; }
+        public string url { get; set; }
+        public string content { get; set; }
+        public bool HasState { get; set; }
+    }
+}
---
+++ VoatHubTests/Api/TokenManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.Web.Http;
+
+using VoatHub.Api;
+
+namespace VoatHubTests.Api
+{
+    [TestClass]
+    public class TokenManagerTests
+    {
+        readonly string tokenUri = "https://fakevout.azurewebsites.net/api/token";
+
+        TokenManager tokenManager;
+        CredentialManager credentialManager;
+        HttpClient httpClient;
+
+        private void setUp()
+        {
+            credentialManager = new CredentialManager("tokenManagerTest");
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Voat-ApiKey", "ZbDlC73ndD6TB84WQmKvMA==");
+        }
+
+        [TestMethod]
+        public void InitializationTest()
+        {
+            setUp();
+            credentialManager.Logout();
+            tokenManager = new TokenManager(tokenUri, httpClient, credentialManager);
+            Assert.AreEqual(tokenManager.AccessToken, null);
+        }
+
+        [TestMethod]
+        public void TokenTest()
+        {
+            setUp();
+            credentialManager.Login("swampfire100", "password");
+
+            tokenManager = new TokenManager(tokenUri, httpClient, credentialManager);
+            Assert.AreNotEqual(tokenManager.AccessToken, null);
+        }
+    }
+}
